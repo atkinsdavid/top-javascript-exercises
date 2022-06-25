d8a50322ae1d3995ab4daba18b4e7bcc6a774f99
David Atkins <david.atkins@me.com>
2022-06-25 09:02:14 -0500

    finished 05, had to get help for isInteger method

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(one,two) {
+    if (!Number.isInteger(one) || !Number.isInteger(two) || one < 0 || two < 0){
+        return 'ERROR';
+    };
+    let sum = 0;
+    if (one > two) {
+        for (i = two; i <= one; i++) {
+            sum += i;
+        };
+    } else {
+        for (i = one; i <= two; i++) {
+            sum += i;
+        };
+    };
+    return sum;
 };
 
 // Do not edit below this line
