c89e1f81fb5047426641318c631478135f2f12cf
David Atkins <david.atkins@me.com>
2022-06-24 22:11:08 -0500

    solutions for 01-03, some dumb fumbling for 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let strArr = string.split('');
+    let revArr = strArr.reverse();
+    let revStr = revArr.join('');
+    return revStr;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(stuff) {
+    let array = Array.from(stuff);
+    for (i = 1; i < array.length; i++) {
+        let comp = array[i];
+        for (j = 0; j < array[0].length; i++) {
+            if (comp == array[j]) {
 
+            }
+        }
+    }
+    
 };
 
 // Do not edit below this line
